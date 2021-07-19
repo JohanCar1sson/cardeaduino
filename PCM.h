d66309f4a9d783c01428be8d657f3d25c0c166a0
Johan Carlsson <johan@uwalumni.com>
2021-07-19 16:49:41 -0400

    Can now loop a sound, using modified PCM library

---
+++ PCM.h
@@ -0,0 +1,44 @@
+/* A version of the PCM library [1] that was modified by Johan Carlsson
+ * (@JohanCar1sson) to allow playing multiple sounds, or the same sound multiple
+ * times. The changes where trivial, so no copyright is claimed.
+ * [1] https://github.com/damellis/PCM
+ */
+
+/*
+ * speaker_pcm
+ *
+ * Plays 8-bit PCM audio on pin 11 using pulse-width modulation (PWM).
+ * For Arduino with Atmega168 at 16 MHz.
+ *
+ * Uses two timers. The first changes the sample value 8000 times a second.
+ * The second holds pin 11 high for 0-255 ticks out of a 256-tick cycle,
+ * depending on sample value. The second timer repeats 62500 times per second
+ * (16000000 / 256), much faster than the playback rate (8000 Hz), so
+ * it almost sounds halfway decent, just really quiet on a PC speaker.
+ *
+ * Takes over Timer 1 (16-bit) for the 8000 Hz timer. This breaks PWM
+ * (analogWrite()) for Arduino pins 9 and 10. Takes Timer 2 (8-bit)
+ * for the pulse width modulation, breaking PWM for pins 11 & 3.
+ *
+ * References:
+ *     http://www.uchobby.com/index.php/2007/11/11/arduino-sound-part-1/
+ *     http://www.atmel.com/dyn/resources/prod_documents/doc2542.pdf
+ *     http://www.evilmadscientist.com/article.php/avrdac
+ *     http://gonium.net/md/2006/12/27/i-will-think-before-i-code/
+ *     http://fly.cc.fer.hr/GDM/articles/sndmus/speaker2.html
+ *     http://www.gamedev.net/reference/articles/article442.asp
+ *
+ * Michael Smith <michael@hurts.ca>
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void pcm_init();
+void pcm_final();
+void pcm_play(unsigned char const *data, int length);
+
+#ifdef __cplusplus
+}
+#endif
--- cardeaduino.ino
+++ cardeaduino.ino
@@ -1,16 +1,27 @@
-#include <PCM.h>
+/* Microcontroller that makes discouraging sounds when a doorknob is touched
+ * https://github.com/JohanCar1sson/cardeaduino
+ */
 
+#include "PCM.h"
+
+//#include "sine1kHz.h"
 //#include "duemilanove.h"
 //#include "shotgun.h"
 #include "dog.h"
 
+unsigned short nloop = 0;
+
 void setup()
 {
-  //startPlayback(sample, sizeof(sample));
-  //startPlayback(shotgun_data, shotgun_length);
-  startPlayback(dog_data, dog_length);
+  pcm_init();
 }
 
 void loop()
 {
+  //pcm_play(sine1kHz_data, 8000);
+  //pcm_play(sample, sizeof(sample));
+  //pcm_play(shotgun_data, shotgun_length);
+  pcm_play(dog_data, dog_length);
+
+  if (3 == ++nloop) pcm_final();
 }
