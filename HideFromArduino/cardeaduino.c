f4e5bb0a237668e9e715d229fc4f7a232224571f
Johan Carlsson <johan@uwalumni.com>
2021-07-28 01:05:59 -0400

    C version fully independent of Arduino software

---
+++ HideFromArduino/cardeaduino.c
@@ -0,0 +1,50 @@
+/* Microcontroller that makes discouraging sounds when a doorknob is touched
+ * https://github.com/JohanCar1sson/cardeaduino
+ */
+
+#include <stdlib.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+
+#include "PCM.h"
+
+/* #include "../sine1kHz.h" */
+/* #include "../duemilanove.h" */
+#include "../shotgun.h"
+#include "../dog.h"
+
+void random_delay()
+{
+  unsigned char n, nsleep;
+
+  /* nsleep = (unsigned char)(rand() >> 8); */ /* Use most-significant byte as an unsigned char */
+  nsleep = (unsigned char)(rand() >> 12); /* Use the 3 most-significant bits to make PRN in range [0, 7] */
+  for (n = 0; n <= nsleep; n++) _delay_ms(1000);
+
+  return;
+}
+
+int main()
+{
+  unsigned char nloop = 0;
+
+  pcm_init();
+  srand(31337);
+
+  while (1)
+  {
+    /* pcm_play(sine1kHz_data, 8000); */
+    /* pcm_play(duemilanove_data, duemilanove_length);
+    _delay_ms(duemilanove_length / 7); */
+    random_delay(); /* Random wait to build suspense */
+    pcm_play(shotgun_data, shotgun_length);
+    /* Wait for the amount of time it takes to play the sound, times 8/7 to be safe */
+    _delay_ms(shotgun_length / 7); /* Remember, there's 8 samples per millisecond */
+    random_delay();
+    pcm_play(dog_data, dog_length);
+    _delay_ms(dog_length / 7);
+
+    if (3 == ++nloop) pcm_final();
+  }
+}
